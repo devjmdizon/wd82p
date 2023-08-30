607dc087315b179684b327a07376aedb0a43d07a
Joseph Michael Dizon <dizonjoseph23@gmail.com>
2023-08-30 11:21:42 +0800

    Add Javascript Folder

---
+++ 15-javascript_fundamentals/main.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 7 * a;
+console.log("4. ");
+console.log(a);
+console.log(9 * a);
+console.log(b);
+
+console.log("1. ", 23 + 97);
+console.log("2. ", 23 + 97 + 32 + 29 + 37 + 79);
+
+console.log("3. ", (4 + 6 + 9) / 77);
+
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log("5. ");
+console.log(percentage);
