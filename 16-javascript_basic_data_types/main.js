0e596ea080c78422df11d025760c0960f5e2b3d5
Joseph Michael Dizon <dizonjoseph23@gmail.com>
2023-09-01 11:13:54 +0800

    Add Javascript Folder and Files

---
+++ 16-javascript_basic_data_types/main.js
@@ -0,0 +1,65 @@
+let age = 28;
+let tempInCelcius = 38.5;
+
+let myAge = parseInt("28");
+let tempInFahrnheit = parseFloat("89.7");
+
+let firstNumber = 10;
+let secondNumber = 5;
+let thirdNumber = parseInt("5");
+
+console.log(6 + 9);
+console.log(20 - 15);
+console.log(3 * 7);
+console.log(10 / 5);
+console.log(5 ** 2); //exponent
+console.log(8 % 3); //remainder
+
+let year = 2023;
+// increment
+year++;
+
+//decrement
+year--;
+
+//assignment (-)
+let x = 3;
+let y = 4;
+x *= y;
+
+console.log(x);
+
+// comparison
+
+let a = 5;
+let b = 2;
+
+console.log(a == b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+
+//2. Strings
+
+let string = "This is a string";
+let single = "Single Quotes";
+let double = "Double Quotes";
+let backtick = `Backtick`;
+
+console.log(typeof single);
+console.log(typeof double);
+console.log(typeof backtick);
+
+console.log("The value of single: " + single);
+
+const name = "Carlo";
+const greeting = `Hello, ${name}`;
+
+console.log(greeting);
+
+const one = "Hello";
+const two = "how are you?";
+const joined = "${one}, ${two}";
+
+console.log("1. ", single);
+console.log(one, two);
