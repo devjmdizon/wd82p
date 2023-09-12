a0424a6d5d9a1267a77b7a7d8b7a4ecc82f0391a
Joseph Michael Dizon <dizonjoseph23@gmail.com>
2023-09-12 12:07:20 +0800

    Add exercises 4 and 5 files

---
+++ 26-asynch_activity_js/exercise_4.js
@@ -0,0 +1,3 @@
+// Problem 4: Find the Largest Number in an Array
+
+// Write a JavaScript function called findLargestNumber that takes an array of numbers as input and returns the largest number in the array.
---
+++ 26-asynch_activity_js/exercise_5.js
@@ -0,0 +1,3 @@
+// Problem 5: Palindrome Checker
+
+// Write a JavaScript function called isPalindrome that checks if a given string is a palindrome. A palindrome is a word, phrase, or sequence of characters that reads the same forwards and backwards, ignoring spaces, punctuation, and capitalization.s
